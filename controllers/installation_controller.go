7f0bc9b832760461a2cc74990f8d0c503ed8ba59
Andrew Lavery <laverya@umich.edu>
2024-02-08 12:26:31 -0500

    reduce logging

--- controllers/installation_controller.go
+++ controllers/installation_controller.go
@@ -308,8 +308,6 @@ func (r *InstallationReconciler) ReconcileHelmCharts(ctx context.Context, in *v1
 		return fmt.Errorf("failed to check chart drift: %w", err)
 	}
 
-	fmt.Printf("detected chart drift: %t\n", chartDrift)
-
 	// detect drift between the cluster config and the installer metadata
 	var installedCharts k0shelm.ChartList
 	if err := r.List(ctx, &installedCharts); err != nil {
@@ -319,8 +317,6 @@ func (r *InstallationReconciler) ReconcileHelmCharts(ctx context.Context, in *v1
 	if err != nil {
 		return fmt.Errorf("failed to check chart completion: %w", err)
 	}
-
-	fmt.Printf("detected pending charts: %+v\n", pendingCharts)
 
 	// If any chart has errors, update installer state and return
 	// if there is a difference between what we want and what we have

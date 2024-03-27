c32d28e66e2b126db7b70a94badae8ef414b9541
Andrew Lavery <laverya@umich.edu>
2024-03-27 22:55:25 +0900

    add a new 'InstallationCompleted' event

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -54,6 +54,11 @@ type UpgradeSucceededEvent struct {
 	ClusterID string `json:"clusterID"`
 }
 
+// InstallationSucceededEvent event is send back home when the upgrade succeeds.
+type InstallationSucceededEvent struct {
+	ClusterID string `json:"clusterID"`
+}
+
 // Hash returns the hash of the node.
 func (n NodeEvent) Hash() (string, error) {
 	hasher := sha256.New()
@@ -123,4 +128,9 @@ func NotifyUpgradeFailed(ctx context.Context, baseURL string, ev UpgradeFailedEv
 // NotifyUpgradeSucceeded notifies the metrics server that an upgrade has succeeded.
 func NotifyUpgradeSucceeded(ctx context.Context, baseURL string, ev UpgradeSucceededEvent) error {
 	return sendEvent(ctx, "UpgradeSucceeded", baseURL, ev)
+}
+
+// NotifyInstallationCompleted notifies the metrics server that an upgrade has started.
+func NotifyInstallationCompleted(ctx context.Context, baseURL string, ev InstallationSucceededEvent) error {
+	return sendEvent(ctx, "InstallationCompleted", baseURL, ev)
 }

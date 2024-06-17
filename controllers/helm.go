2793a3eec111c6a91467274db85e8876687e8fa1
Josh De Winne <joris.dewinne@gmail.com>
2024-06-17 14:16:33 -0700

    Add proxy support to Velero

--- controllers/helm.go
+++ controllers/helm.go
@@ -204,6 +204,18 @@ func updateInfraChartsFromInstall(ctx context.Context, in *v1beta1.Installation,
 
 			charts[i].Values = newVals
 		}
+		if chart.Name == "velero" {
+			if in.Spec.Proxy != nil {
+				extraEnv := getExtraEnvFromProxy(in.Spec.Proxy.HTTPProxy, in.Spec.Proxy.HTTPSProxy, in.Spec.Proxy.NoProxy)
+				newVals, err := setHelmValue(chart.Values, "extraEnvVars", extraEnv)
+				if err != nil {
+					log.Error(err, "failed to set helm values extraEnvVars", "chart", chart.Name)
+					continue
+				}
+				charts[i].Values = newVals
+			}
+
+		}
 	}
 	return charts
 }

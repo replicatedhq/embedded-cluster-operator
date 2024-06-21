c9a9fa17b9634050100221f40783030bac36890a
Ethan Mosbaugh <ethan@replicated.com>
2024-06-21 11:06:41 -0700

    f

--- pkg/upgrade/upgrade.go
+++ pkg/upgrade/upgrade.go
@@ -42,6 +42,10 @@ func Upgrade(ctx context.Context, cli client.Client, in *clusterv1beta1.Installa
 		// in airgap installations we need to copy the artifacts to the nodes and then autopilot
 		// will copy the images to the cluster so we can start the new operator.
 
+		if localArtifactMirrorImage == "" {
+			return fmt.Errorf("local artifact mirror image is required for airgap installations")
+		}
+
 		err := metadata.CopyVersionMetadataToCluster(ctx, cli, in)
 		if err != nil {
 			return fmt.Errorf("copy version metadata to cluster: %w", err)
